ebad54d31217ba2c14f36ea51587ca4767f5df23
Alex Sousa <alxroots@gmail.com>
2020-11-09 07:33:00 -0300

    finished challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,12 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -11,18 +18,38 @@ class TransactionsRepository {
 
   constructor() {
     this.transactions = [];
+
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce((accumulator: Balance, transaction: Transaction) => {
+      switch (transaction.type) {
+        case 'income':
+          accumulator.income += transaction.value;
+          break;
+        case 'outcome':
+          accumulator.outcome += transaction.value;
+        default:
+          break;
+      }
+      return accumulator;
+    }, {
+      income: 0,
+      outcome:0,
+      total: 0
+    })
+    const total = income - outcome;
+    return {income, outcome, total};
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type})
+    this.transactions.push(transaction)
+    return transaction
   }
 }
 
